d60bd2d83f5f4be62ce9c5c65d7a1484c26595ad
Gleb Lysov <lysovg9@gmail.com>
2020-05-02 06:37:26 +0300

    v0.1.2: finally handling input

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,23 @@
-use tokio::net::{TcpListener, TcpStream};
-use tokio::sync::{mpsc, Mutex};
-use tokio::io::{AsyncRead, AsyncWriteExt, AsyncReadExt};
-use tokio::stream::{Stream, StreamExt};
 
-use std::io;
-use std::collections::HashMap;
-use std::task::{Poll, Context};
-use std::pin::Pin;
-use std::process;
+use tokio::stream::StreamExt;
+use tokio::net::{TcpListener, TcpStream};
+use std::net::SocketAddr;
+use tokio::sync::{mpsc};
 use std::env;
 use std::error::Error;
-use std::net::SocketAddr;
 use std::sync::Arc;
 use std::string::String;
-use std::vec::Vec;
-use std::convert::From;
 use serde_json::{Value};
-use serde_repr::*;
-use serde::{Serialize, Deserialize};
+use tokio::task;
+
+mod peer;
+mod server;
+mod message;
+mod input;
+use peer::*;
+use server::*;
+use message::*;
+use input::*;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
@@ -26,7 +26,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
     // The server task will hold a handle to this. For every new client, the
     // `server` handle is cloned and passed into the task that processes the
     // client connection.
-    let servers = Shared::new();
+    let servers = Arc::new(Shared::new());
 
     let addr = env::args()
         .nth(1)
@@ -38,20 +38,17 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let mut listener = TcpListener::bind(&addr).await?;
     println!("server running on {}", addr);
 
-//for debug settin 3 servers
+    //for debug settin 7 servers
     for i in 0..6 {
         let name = format!("server {}", i);
         servers.add_server(Arc::new(Server::new(name))).await;
     }
+    // let servers_clone = servers.clone();
+    // task::spawn(async move {
+    //     input_process_control(servers_clone).await;
+    // });
+    input_process_control(servers.clone()).await;
     println!("running {} servers", servers.len().await);
-
-    let servers = Arc::new(servers);
-        
-    let servers_clone = servers.clone();
-
-    tokio::spawn(async move {
-        input_process(servers_clone).await;
-    });
 
     loop {
         // Asynchronously wait for an inbound TcpStream.
@@ -60,73 +57,11 @@ async fn main() -> Result<(), Box<dyn Error>> {
         let servers_clone = servers.clone();
 
         // Spawn our handler to be run asynchronously.
-        tokio::spawn(async move {
+        task::spawn(async move {
             if let Err(e) = process(servers_clone, stream, addr).await {
                 println!("an error occurred; error = {:?}", e);
             }
         });
-    }
-}
-
-async fn input_process(servers: Arc<Shared>) {
-    let stdin = io::stdin();
-    loop {
-        let mut buffer = String::new();
-        match stdin.read_line(&mut buffer) {
-            Ok(_) => (),
-            Err(e) => {
-                println!("{:?}", e);
-                continue;
-            },
-        }
-        let mut iter = buffer.split_whitespace();
-        match iter.next().unwrap_or("") {
-            "add" | "a" => {
-                match iter.next() {
-                    Some(v) => {
-                        servers.add_server(Arc::new(Server::new(v.into()))).await;
-                        // UPDATE SERVERS
-                        // to work on
-                        let msg = PeerMessage{
-                            message_type:MessageType::ServersList, 
-                            message:servers.servers_to_arr().await,
-                        };
-                        let msg = serde_json::to_string(&msg).unwrap();
-                        servers.broadcast_all(&msg).await;
-                        
-                    },
-                    None => {
-                        println!("server name missing");
-                    },
-                }
-            },
-            "servers" | "s" => {
-                let servers = servers.servers.lock().await;
-                println!("Available servers:");
-                servers.iter().for_each(|x| println!("{}", x.name));
-            },
-            "Stop" | "S" => process::exit(1),
-            "peers" | "p" => {
-                let servers = servers.servers.lock().await;
-                let mut iter = servers.iter();
-                while let Some(v) = iter.next() {
-                    println!("Peers for \"{}\"", v.name);
-                    let peers = v.peers.lock().await;
-                    peers.iter().for_each(|x| print!("{}; ", x.0));
-                    println!("");
-                }
-            },
-            "help" | "h" => {
-                println!("Available commands:");
-                println!("(a)dd <arg>       -print to add server, where arg - server name");
-                println!("(p)eers           -print to display connected peers (curenntly on all servers)");
-                println!("(s)ervers         -print to display available servers");
-                println!("(S)top            -print to stop current process");
-                println!("(h)elp            -print to show this message");
-            },
-            "" => continue,
-            &_ => print!("{}", buffer),
-        }
     }
 }
 
@@ -135,231 +70,6 @@ type Tx = mpsc::UnboundedSender<String>;
 
 /// Shorthand for the receive half of the message channel.
 type Rx = mpsc::UnboundedReceiver<String>;
-
-struct Shared {
-        //rework
-        servers: Mutex<Vec<Arc<Server>>>,
-}
-
-impl Shared {
-        //rework
-    fn new() -> Self {
-        Shared {
-            servers: Mutex::new(Vec::new()),
-        }
-    }
-    async fn add_server(&self, server: Arc<Server>) {
-        self.servers.lock().await.push(server);
-    }
-    async fn choose_server(&self, pos: u32) -> Option<Arc<Server>> {
-        let servers = self.servers.lock().await;
-        return match servers.get(pos as usize) {
-            Some(server) => Some(server.clone()),
-            None => None
-        };
-    }
-    async fn len(&self) -> usize {
-        return self.servers.lock().await.len();
-    }
-    async fn servers_to_arr(&self) -> Vec<String> {
-        let servers = self.servers.lock().await;
-        let json_vec:Vec<String> = servers.iter().map(|x| String::from(x.name.as_str())).collect();
-        return json_vec;
-    }
-    async fn broadcast_all(&self, msg: &str) {
-        let servers = self.servers.lock().await;
-        for server in servers.iter() {
-            server.broadcast_all(msg).await;
-        }
-    }
-}
-
-#[derive(Debug)]
-struct Server {
-        name: String,
-        peers: Mutex<HashMap<SocketAddr, Tx>>,
-}
-
-#[allow(dead_code)]
-impl Server {
-        //rework
-        fn new(name: String) -> Self {
-            Server {
-                name: name,
-                peers: Mutex::new(HashMap::new()),
-            }
-        }
-        //rework to peer
-        async fn push_new_peer(&self, addr: SocketAddr, tx: Tx) {
-            //pushing address
-            let mut sync_peers = self.peers.lock().await;
-            sync_peers.insert(addr, tx);
-            println!("pushing new peer: ({})", addr);
-            //println!("new length of peers vec: {}", sync_peers.len());
-        }
-        async fn remove_by_addr(&self, addr: SocketAddr) -> Result<Tx, Box<dyn Error>> {
-            println!("removing peer {}", addr);
-            let mut sync_peers = self.peers.lock().await;
-            //println!("old length of peers vec: {}", sync_peers.len());
-            match sync_peers.remove(&addr) {
-                Some(v) => {
-                    println!("succesfully removed {}", addr);
-                    return Ok(v);
-                },
-                None => {
-                    println!("error on removing");
-                    return Err("did not find peer".into());
-                },
-            }
-            //println!("new length of peers vec: {}", sync_peers.len());
-        }
-        
-        async fn len(&self) -> usize {
-            return self.peers.lock().await.len();
-        }
-        async fn broadcast_all(&self, msg: &str) {
-            let sync_peers = self.peers.lock().await;
-            for peer in sync_peers.iter() {
-                let _ = peer.1.send(msg.to_string());
-                println!("debug broadcast_all() : {:?}", peer.1);
-            }
-        }
-
-        async fn broadcast(&self, sender: SocketAddr, msg: &str) {
-            let sync_peers = self.peers.lock().await;
-            for peer in sync_peers.iter() {
-                if *peer.0 != sender {
-                    let _ = peer.1.send(msg.to_string());
-                }
-            }
-        }
-}
-
-enum Message {
-    Received(String),
-    Broadcast(String),
-}
-
-//MAKE DUMMY PEER
-struct DummyPeer {
-    stream: TcpStream,
-}
-
-struct Peer {
-    stream: TcpStream,
-    rx: Rx,
-    //rx: Option<Rx>,
-}
-
-impl DummyPeer {
-    async fn new(stream: TcpStream) -> Self {
-        DummyPeer {
-            stream: stream,
-        }
-    }
-    async fn write_stream(&mut self, msg: &str) {
-        self.stream.write_all(msg.as_bytes()).await.expect("failed to write to peers");
-    }
-    async fn read_stream(&mut self) -> Option<String> {
-        let mut buf = [0;512]; //NEED TO CHANHGE THIS BUFFER
-        let n = match self.stream.read(&mut buf).await {
-            Ok(n) => n,
-            Err(_) => {return None;},
-        };
-        if n==0 {return None;}
-        let msg = String::from_utf8(buf[0..n].to_vec()).unwrap();
-        return Some(msg);
-    }
-}
-
-impl Peer {
-    async fn new(server: Arc<Server>, peer: DummyPeer) -> Self {
-        let addr = peer.stream.peer_addr().expect("failed to get addr");
-
-        // Create a channel for this peer
-        let (tx, rx) = mpsc::unbounded_channel();
-
-        server.push_new_peer(addr, tx).await;
-        Peer {
-            stream: peer.stream,
-            rx: rx,
-        }
-    }
-    async fn write_stream(&mut self, msg: &str) {
-        self.stream.write_all(msg.as_bytes()).await.expect("failed to write to peers");
-    }
-    
-    async fn change_server(&mut self, addr: SocketAddr, server_old: Arc<Server>, server_new: Arc<Server>) {
-        let tx = server_old.remove_by_addr(addr).await.unwrap();
-        server_new.push_new_peer(addr, tx).await;
-        println!("moving peer {} to {}", addr, server_new.name);
-    }
-}
-
-impl Stream for Peer {
-//    type Item = Result<(String, usize), Box<dyn Error>>;
-    type Item = Message;
-
-    fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
-
-        if let Poll::Ready(Some(v)) = Pin::new(&mut self.rx).poll_next(cx) {
-            return Poll::Ready(Some(Message::Received(v)));
-        }
-
-        let mut buf = [0;512]; //NEED TO CHANHGE THIS BUFFER
-        let result = futures::ready!(Pin::new(&mut self.stream).poll_read(cx, &mut buf));
-
-        Poll::Ready( match result {
-            Ok(v) => {
-                if v == 0 { None }
-                else {
-                    let msg = String::from_utf8(buf[0..v].to_vec()).unwrap();
-                    Some(Message::Broadcast(msg))
-                }
-            },
-            Err(e) => {
-                println!("error on read {:?}", e);
-                None
-            },
-        })
-    }
-
-}
-
-#[derive(Serialize_repr, Deserialize_repr, PartialEq, Debug)]
-#[repr(u8)]
-enum MessageType {
-    ServerChoice, //0
-    Message, //1
-    ServersList,
-    ServersStatus, //setup for fetching status
-
-    Unknown,
-}
-
-impl From<u64> for MessageType {
-    fn from(v: u64) -> Self {
-        match v {
-            0 => MessageType::ServerChoice,
-            1 => MessageType::Message,
-            2 => MessageType::ServersList,
-            3 => MessageType::ServersStatus,
-            _ => MessageType::Unknown,
-        }
-    }
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-struct PeerMessageText {
-    name: String,
-    data: String,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-struct PeerMessage<T> {
-    message_type: MessageType,
-    message: T,
-}
 
 /// Process an individual chat client
 async fn process(
@@ -401,11 +111,8 @@ async fn process(
     // println!("connected {} on {}", username, addr);
 
         loop {
-            let e = peer.rx.try_recv();
-            println!("sstar waiting{:?}", e);
             match peer.next().await {
                 Some(Message::Broadcast(msg)) => {
-                    println!("1) got msg {:?} on {}", msg, username);
                     let msg: Value = match serde_json::from_str(&msg) {
                         Ok(v) => v,
                         Err(e) => {
@@ -453,7 +160,6 @@ async fn process(
                     };
                 },
                 Some(Message::Received(msg)) => {
-                    println!("2) got msg {:?} on {}", msg, username);
                     peer.write_stream(&msg).await;
                 },
                 None => {
